348f733cbe93ecaa2bdff167ccb932b365b5fb3f
thaonhu24 <thaonhu1240@gmail.com>
2021-05-26 21:02:46 +0700

    add registerPage

---
+++ blog/footer/footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SWEET</title>
+<link rel="stylesheet" href="../footer/footer.css">
+<link rel="stylesheet" href="../footer/themify-icons/">
+</head>
+<body>
+
+    <div id="footer">
+    <div id="div-left-footer">
+    <div class="space-footer"></div>    
+    <div class="space-footer"></div>
+    <div class="div-left-footer">
+    <div id="address-left-footer">
+        <div id="posi-address-left-footer">
+        <h2>LIÊN HỆ</h2>
+        <p>Gmail: sweetbakery@gmail.com</p>
+        <p>Địa chỉ: 22 Trần Thị Nghĩa, Gò Vấp, TPHCM</p>
+        <p>SDT: 0706811338</p>
+        </div>
+    </div>
+    <div id="img-contact-left-footer">
+    <h2>FOLLOW ME</h2>
+    <ul id="list-follow-me-left-footer">
+<li><img class="img-follow-me-left-footer" style="background-image: url(../footer/img/facebook.png); background-repeat: no-repeat;border-radius: 20px;"></li>
+<li><img class="img-follow-me-left-footer" style="background-image: url(../footer/img/instar.png);background-repeat: no-repeat;border-radius: 20px;"></li>
+<li><img class="img-follow-me-left-footer" style="background-image: url(../footer/img/images.png);background-repeat: no-repeat;background-size: 40px;border-radius: 20px;"></li>
+    </ul>
+    </div>
+    <div class="footer-clear"></div>
+    </div>
+    </div>
+    <div id="div-right-footer">
+    <div class="space-footer"></div>    
+    <div class="space-footer"></div>
+    <div id="posi-div-right-footer">   
+    <div id="div-menu-right-footer">
+        <ul id="list-menu-right-footer">
+            <li class="li-list-menu-right-footer">HOME</li>
+            <div class="space-footer"></div>   
+            <li class="li-list-menu-right-footer">MENU</li>
+            <div class="space-footer"></div>   
+            <li class="li-list-menu-right-footer">BLOG</li>
+            <div class="space-footer"></div>   
+            <li class="li-list-menu-right-footer">SỰ KIỆN</li>
+        </ul>
+    </div>
+    <div id="div-map-right-footer">
+        <img id="img-map-right-footer"src="../footer/img/map.jpg" alt="">
+    </div>
+    <div class="footer-clear"></div></div>
+    </div>
+    <div class="footer-clear"></div>
+    </div>
+</body>
+</html>
---
+++ blog/header/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SWEET</title>
+<link rel="stylesheet" href="../header/header.css">
+<link rel="stylesheet" href="../header/themify-icons/themify-icons.css">
+</head>
+<body>
+<div id="head-header">
+<div id="div-time-head-header">
+
+<div class="header-space"></div>
+<div class="div-time-head-headers">
+<div id="time-open-head-header">OPEN TIME: 8:00AM</div>
+<div id="time-close-head-header">CLOSE TIME: 8:PM</div>
+<div class="header-clear"></div>
+</div>
+</div>
+<div id="div-buton-head-header">
+<div class="header-space"></div>
+    <div class="div-buton-head-header">
+        <div id="div-button-startup-head-header">
+            <button id="button-startup-head-header">Đăng nhập</button>
+        </div>
+        <div id="div-button-startin-head-header">
+            <button id="button-startin-head-header">Đăng kí</button>
+        </div>
+        <div class="header-clear"></div>
+    </div>
+</div>
+</div>
+</body>
+</html>
